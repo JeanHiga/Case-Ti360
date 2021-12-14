b1d067bda4b279fa2ec7a05e3038b8883baf1cec
Jean Eduardo Higa <jeaneduardohiga@gmail.com>
2021-12-14 08:19:19 -0300

    primeiro commit

---
+++ src/test/java/com/example/ti360/college/CollegeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ti360.college;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
