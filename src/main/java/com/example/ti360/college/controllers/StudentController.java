bb53f675ecc1a75e19b44d73a178444086cd5694
Jean Eduardo Higa <jeaneduardohiga@gmail.com>
2021-12-16 11:09:43 -0300

    inclusão rota put

--- src/main/java/com/example/ti360/college/controllers/StudentController.java
+++ src/main/java/com/example/ti360/college/controllers/StudentController.java
@@ -4,6 +4,7 @@ import com.example.ti360.college.models.Student;
 import com.example.ti360.college.services.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -26,4 +27,13 @@ public class StudentController {
         return studentService.findAllStudents();
     }
 
+    @GetMapping("/{name}")
+    public List<Student> findByName(@PathVariable String name){
+        return studentService.findByName(name);
+    }
+
+    @PutMapping
+    public ResponseEntity<Student> replace(@RequestBody Student student){
+        return studentService.replace(student);
+    }
 }
--- src/main/java/com/example/ti360/college/services/StudentService.java
+++ src/main/java/com/example/ti360/college/services/StudentService.java
@@ -3,6 +3,7 @@ package com.example.ti360.college.services;
 import com.example.ti360.college.models.Student;
 import com.example.ti360.college.repositories.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -21,5 +22,15 @@ public class StudentService {
 
     public List<Student> findAllStudents() {
         return studentRepository.findAll();
+    }
+
+    public List<Student> findByName(String name) {
+        return studentRepository.findByName(name);
+    }
+
+    public ResponseEntity<Student> replace(Student student) {
+        studentRepository.save(student);
+
+        return ResponseEntity.ok(student);
     }
 }
